47dc5cb07dc740c5e11c6a40b4ae34e5083f76ed
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2023-08-07 10:43:39 +0530

    icons added and price also

--- about-cards/src/components/products.js
+++ about-cards/src/components/products.js
@@ -1,10 +1,22 @@
-// import { FaShoppingCart, FaRegBookmark, FaStar, FaFireAlt } from 'react-icons/fa';
+import { FaShoppingCart, FaRegBookmark, FaStar, FaFireAlt } from 'react-icons/fa';
 
 export function Products(props) {
     return(
         <div className='ProductList'>
-            <div key={props.id} className="product_card"></div>
-                <img src={props.image} alt='product_image' className='produtImage'></img>
+            <div key={props.id} className="productCard"></div>
+                <img src={props.image} alt='product-imagess' className='produtImage'></img>
+
+                <FaShoppingCart className={"productCard__cart"}/>
+                <FaRegBookmark className={"productCard__wishlist"}/>
+                <FaFireAlt className={"productCard__fastSelling"}/>
+
+                <div className='productCard__content'>
+                    <h3 className='productName'>{props.name}</h3>
+                    <div className='displayStack__1'>
+                        <div className='productPrice'>${props.price}</div>
+                        <div className='productSales'>{props.totalSales} unit solds</div>
+                    </div>
+                </div>
         </div>
     );
 }
