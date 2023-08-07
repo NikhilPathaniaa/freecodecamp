48978fff167b784c19a515c4e15c7690b6b5e968
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2023-08-07 10:51:19 +0530

    all functionality is completed

--- about-cards/src/App.js
+++ about-cards/src/App.js
@@ -1,26 +1,20 @@
-import './App.css';
 import { Products } from './components/products';
 import contents from './content';
 
-function App() {
-  return (
-    <>
-    <h1 className='header'>AMAZON</h1>
-    <div className='App'>
-      {contents.map(contents =>(
-        <Products
-            Key={contents.id}
-            image={contents.image}
-            name={contents.name}
-            price={contents.price}
-            totalSales={contents.totalSales}
-            timeLeft={contents.timeLeft}
-            rating={contents.rating}
-        />
-      ))}
-    </div>
-    </>
-  );
+export default function App() {
+    return(
+           <div className='App'>
+               {contents.map(contents => (
+                   <Products 
+                       key={contents.id}
+                       image={contents.image}
+                       name={contents.name}
+                       price={contents.price}
+                       totalSales={contents.totalSales}
+                       timeLeft={contents.timeLeft}
+                       rating={contents.rating}
+                   />
+               ))}
+           </div>
+    )
 }
-
-export default App;
