c1b92b3ba2dd30f0b3e82bfde6f28de5ccba61ad
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2023-08-07 06:44:23 +0530

    no no everything is wrong

--- about-cars/src/components/Car.js
+++ about-cars/src/components/Car.js
@@ -1,7 +1,8 @@
 function Car(props) {
   return (
     <div className="car">
-      <h1>My {props.make}</h1>
+      <img src={props.imgUrl} />
+      <h2>My {props.make}</h2>
       <p>
         My {props.make} is {props.color}
       </p>
@@ -11,9 +12,32 @@ function Car(props) {
 }
 
 let app = (
-  <div>
-    <Car make="Honda" color="red" year="2011" />
+  <div class="cars">
+    <Car
+      make="BMW"
+      color="blue"
+      year="2011"
+      imgUrl="https://www.codingexercises.com/img/2022-01-25/001-bmw-blue.jpg"
+    />
+    <Car
+      make="Mercedes"
+      color="gray"
+      year="2018"
+      imgUrl="https://www.codingexercises.com/img/2022-01-25/002-mercedes-gray.jpg"
+    />
+    <Car
+      make="Toyota"
+      color="gray"
+      year="new"
+      imgUrl="https://www.codingexercises.com/img/2022-01-25/003-toyota-gray.jpg"
+    />
+    <Car
+      make="Alpha Romeo"
+      color="red"
+      year="2012"
+      imgUrl="https://www.codingexercises.com/img/2022-01-25/004-alpha-romeo-red.jpg"
+    />
   </div>
 );
 
-ReactDOM.render(app, document.querySelector("#app"));
+ReactDOM.render(app, document.querySelector("#app")); 
