b96d61a1e291d233f1061205ed38be39f7806ed9
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2023-08-07 10:32:38 +0530

    add images and add heading

--- about-cards/src/App.js
+++ about-cards/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Products } from './components/products';
+import contents from './content';
 
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+    <h1 className='header'>AMAZON</h1>
+    <div className='App'>
+      {contents.map(contents =>(
+        <Products
+            Key={contents.id}
+            image={contents.image}
+        />
+      ))}
+    </div>
     </>
   );
 }
--- about-cards/src/components/products.js
+++ about-cards/src/components/products.js
@@ -1,8 +1,10 @@
-import { FaShoppingCart, FaRegBookmark, FaStar, FaFireAlt } from 'react-icons/fa';
+// import { FaShoppingCart, FaRegBookmark, FaStar, FaFireAlt } from 'react-icons/fa';
 
 export function Products(props) {
     return(
-        <>
-        </>
+        <div className='ProductList'>
+            <div key={props.id} className="product_card"></div>
+                <img src={props.image} alt='product_image' className='produtImage'></img>
+        </div>
     );
 }
