4f1744fb6732a5116961ffd8f8a2852ca5e196be
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2023-08-07 06:32:14 +0530

    details added

--- about-cars/src/App.js
+++ about-cars/src/App.js
@@ -5,15 +5,38 @@ function App() {
     <>
       <div className='card'>
         <img src="https://www.codingexercises.com/img/2022-01-25/001-bmw-blue.jpg" />
+        <h1>My BMW</h1>
+        <p>
+          My BMW is blue.
+        </p>
+        <p>It's a bit older, from 2011.</p>
       </div>
+
       <div className="card">
         <img src="https://www.codingexercises.com/img/2022-01-25/002-mercedes-gray.jpg" />
+        <h1>My Mercedes</h1>
+        <p>
+          My Mercedes is gray.
+        </p>
+        <p>It's a bit newer, from 2018.</p>
       </div>
+
       <div className="card">
         <img src="https://www.codingexercises.com/img/2022-01-25/003-toyota-gray.jpg" />
+        <h1>My Toyota</h1>
+        <p>
+          My Toyota is gray.
+        </p>
+        <p>It's brand new.</p>
       </div>
+
       <div className="card">
         <img src="https://www.codingexercises.com/img/2022-01-25/004-alpha-romeo-red.jpg" />
+        <h1>My Alpha Romeo</h1>
+        <p>
+          My Alpha Romeo is red.
+        </p>
+        <p>It's a bit older, from 2012.</p>
       </div>
     </>
   );
