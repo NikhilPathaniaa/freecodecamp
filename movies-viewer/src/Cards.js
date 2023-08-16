725a3467dd06fe464d4fdbddc8353ac2271b4039
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2023-08-17 04:27:57 +0530

    add css

--- movies-viewer/src/Cards.js
+++ movies-viewer/src/Cards.js
@@ -2,13 +2,14 @@ import React from 'react'
 
 export default function Cards(props) {
   return (
-    <div className='container'>
-        <img src={props.imagesUrl} alt='...'/>
-        <div className="info">
-            <h1>{props.name}</h1>
-            <p>{props.description} </p><br/>
+    <div className="cards">
+        <div className='card'>
+            <img src={props.imagesUrl} alt='...'/>
+            <div className="info">
+                <h1>{props.name}</h1>
+                <p>{props.description} </p><br/>
+            </div>
         </div>
-        
-    </div>
+    </div>  
   )
 }
