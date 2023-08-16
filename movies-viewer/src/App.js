d40a98a0b6a32380a35174536369f7b982cdbf65
Nikhil Pathania <132260288+NikhilPathaniaa@users.noreply.github.com>
2023-08-17 04:25:12 +0530

    two movie added raid heroin

--- movies-viewer/src/App.js
+++ movies-viewer/src/App.js
@@ -10,9 +10,9 @@ function App() {
       description="A female superstar struggles through the trials and tribulations of being a Bollywood actress."
       />
       <Cards 
-      imagesUrl ="https://flxt.tmsimg.com/assets/p9359629_p_v10_aa.jpg"
-      name ="Heroine"
-      description="A female superstar struggles through the trials and tribulations of being a Bollywood actress."
+      imagesUrl ="https://upload.wikimedia.org/wikipedia/en/2/28/Ajay_Devgn%27s_Raid_poster.jpg"
+      name ="Raid"
+      description=" Income-tax raid conducted by the officers of Income Tax Department on Sardar Inder Singh in the 1980s which distinguished itself from others for being the longest raid in Indian history, lasting 3 days and 2 nights"
       />
     </div>
   );
